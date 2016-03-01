22c721d8c54696ba8b3fa97f40dd68e62be5fa61
David Snyder <dsnyder@b01.clsp.jhu.edu>
2016-03-01 16:12:52 -0500

    xvector: simplifying nnet-xvector-compute.cc

--- src/xvector/nnet-xvector-compute.cc
+++ src/xvector/nnet-xvector-compute.cc
@@ -74,27 +74,10 @@ void NnetXvectorComputer::GetComputationRequest(
   request->inputs[0].has_deriv = false;
 
   // We only need the output on frame t=0.
-  int32 io_index_size = request->inputs[0].indexes.size(),
-         n_indx_size = 1e6, t_ind;
   std::vector<Index> output_indexes;
-
-  std::map<int32, int32> n_indx_sizes;
-  for (int32 indx = 0; indx < io_index_size; indx++) {
-    t_ind = request->inputs[0].indexes[indx].t;
-    if (n_indx_sizes.count(t_ind) != 0)
-      n_indx_sizes[t_ind] += 1;
-    else
-      n_indx_sizes.insert(std::make_pair(t_ind, 1));
-  }
-  std::map<int32, int32>::const_iterator iter;
-  for (iter = n_indx_sizes.begin(); iter != n_indx_sizes.end(); iter++)
-    n_indx_size = std::min(n_indx_size, iter->second);
-
-  output_indexes.resize(n_indx_size);
-  for (int32 indx = 0; indx < n_indx_size; indx++) {
-    output_indexes[indx].n = indx;
-    output_indexes[indx].t = 0;
-  }
+  output_indexes.resize(1);
+  output_indexes[0].n = 0;
+  output_indexes[0].t = 0;
 
   // Add an io_spec for the output node.
   int32 output_node_index = nnet_->GetNodeIndex(output_name);
