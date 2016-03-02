133d0d86c5cb198916fc71a876e4bc34dc0512da
David Snyder <dsnyder@b01.clsp.jhu.edu>
2016-03-02 15:08:15 -0500

    xvector: fixing typo in nnet3-xvector-scoring.cc

--- src/xvectorbin/nnet3-xvector-scoring.cc
+++ src/xvectorbin/nnet3-xvector-scoring.cc
@@ -27,7 +27,7 @@
 
 int main(int argc, char *argv[]) {
   using namespace kaldi;
-    using namespace kaldi::nnet3;
+  using namespace kaldi::nnet3;
   typedef kaldi::int32 int32;
   typedef kaldi::int64 int64;
   try {
